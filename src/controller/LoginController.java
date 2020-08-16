11fda1b8285eb99389b15d1aeb58471d3f1a80ae
Dimi Silva <jnn.sla@gmail.com>
2020-08-16 18:10:59 -0300

    login development

---
+++ src/controller/LoginController.java
@@ -0,0 +1,47 @@
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class LoginController implements Initializable {
+
+	@FXML
+	private VBox vboxLoginContainer;
+	private double vboxLoginContainerXOffset;
+	private double vboxLoginContainerYOffset;
+	
+	@FXML
+	private AnchorPane anchorPaneTopBar;
+	
+	@FXML
+	private ImageView imageViewClose;
+	
+	@FXML
+	public void anchorPaneTopBarOnDragEntered() {
+		System.out.println("teste");
+	} 
+	
+	@FXML
+	void anchorPaneTopBarOnDragDropped () {
+		
+	}
+	
+	@FXML
+	public void imageViewCloseOnClick() {
+		Platform.exit();
+		System.exit(0);
+	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/controller/MainController.java
@@ -0,0 +1,27 @@
+package controller;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class MainController extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/view/LoginView.fxml"));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        Scene scene = new Scene(root);
+        scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
+        		
+        primaryStage.setTitle("EMS Dolphin");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
